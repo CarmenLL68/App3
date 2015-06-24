45f485df3cf8d47123e9ca28c325925c754e49f1
CarmenLL68 <carmen_68@live.com.my>
2015-06-24 19:25:18 +0800

    new commit 1

---
+++ src/app3/App3.java
@@ -0,0 +1,18 @@
+
+package app3;
+
+
+public class App3 {
+
+    
+    public static void main(String[] args) {
+        int a = 3, b = 2, c = 1;
+        
+        if (a > c){
+            a = b = c = 1;
+
+            System.out.println(b);
+
+        }
+    }
+}
